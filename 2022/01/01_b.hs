f790df427830113a9cca093dcd74360be77e7c7e
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2022-12-02 16:18:52 +0100

    Advent Of Code 2022 - Day 02 - Haskell

--- 2022/01/01_b.hs
+++ 2022/01/01_b.hs
@@ -3,13 +3,12 @@ import Data.List
 main = do
     content <- readFile "01_input.txt"
     let linesOfFile = lines content
-    let listOfSums = getSums (head linesOfFile) (tail linesOfFile) 0
-    print(getTop3Sum listOfSums)
+    print(getTop3Sum $ getSums linesOfFile 0)
 
-getSums :: String -> [String] -> Integer -> [Integer]
-getSums _ [] currentSum = [currentSum]
-getSums "" arr currentSum = currentSum : getSums (head arr) (tail arr) 0
-getSums str arr currentSum = getSums (head arr) (tail arr) (currentSum + read(str))
+getSums :: [String] -> Integer -> [Integer]
+getSums [] currentSum = [currentSum]
+getSums ("":arr) currentSum = currentSum : getSums arr 0
+getSums (line:arr) currentSum = getSums arr (currentSum + read(line))
 
 getTop3Sum :: [Integer] -> Integer
 getTop3Sum arr = sum $ take 3 $ reverse $ sort arr
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent Of Code
 ## Advent Of Code 2022
 ### Day 01: Haskell - DONE
+### Day 02: Haskell - DONE
